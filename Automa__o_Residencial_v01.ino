0eb654e8714316f3e5ad0f8f13748344c5cc367a
Lucas Seiti Taba <seiti.taba@gmail.com>
2016-07-06 22:46:57 -0400

    Add files via upload

---
+++ Automa__o_Residencial_v01.ino
@@ -0,0 +1,31 @@
+#define LED_VERDE 7
+#define LED_AMARELO 8
+int comando = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(LED_AMARELO, OUTPUT);
+
+}
+
+void loop() {
+  if (Serial.available()) {
+    comando = Serial.read();
+    Serial.println(comando);
+  }
+  switch (comando) {
+    case 5:
+    digitalWrite(LED_VERDE, HIGH);
+      break;
+    case 6:
+    digitalWrite(LED_VERDE, LOW);
+      break;
+    case 7:
+    digitalWrite(LED_AMARELO, HIGH);
+      break;
+    case 8:
+    digitalWrite(LED_AMARELO, LOW);
+      break;
+  }
+}
